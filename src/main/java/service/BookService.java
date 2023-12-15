e3060d267fcfe2f46759a2ea08e897cdecf9cd92
victus <zlotfi758@gmail.com>
2023-12-15 20:28:30 +0330

    edit

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -15,7 +15,7 @@ public class BookService {
     public void addBook(Book book) throws SQLException {
         int result = bookRepository.save(book);
         if (result != 0)
-            System.out.println(book.getTitle() + "successfully added to database");
+            System.out.println(book.getTitle() + " successfully added to database");
         else
             System.out.println("ERROR 404");
     }
