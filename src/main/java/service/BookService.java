4f5e45b4b375ecc0b9a20ac3bd75f75f6b8a41dd
victus <zlotfi758@gmail.com>
2023-12-15 20:34:25 +0330

    add load method

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -4,8 +4,11 @@ import model.Book;
 import repository.BookRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class BookService {
+
+    Scanner scanner = new Scanner(System.in);
 
     private final BookRepository bookRepository = new BookRepository();
 
@@ -18,5 +21,12 @@ public class BookService {
             System.out.println(book.getTitle() + " successfully added to database");
         else
             System.out.println("ERROR 404");
+    }
+
+    public Book load(int id) throws SQLException {
+        System.out.println("book_id: ");
+        int book_id = scanner.nextInt();
+        Book book = bookRepository.load(book_id);
+        return book;
     }
 }
