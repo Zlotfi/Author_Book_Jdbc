b82a16db10113cd5dd03a4ab517fa404a1e49391
victus <zlotfi758@gmail.com>
2023-12-15 20:21:59 +0330

    add save method

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -1,4 +1,27 @@
 package repository;
 
+import connection.JdbcConnection;
+import model.Book;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class BookRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public BookRepository() throws SQLException {
+    }
+
+    public int save(Book book) throws SQLException {
+        String add = "INSERT INTO book (title, print_year, author_name) VALUES (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1,book.getTitle());
+        preparedStatement.setString(2,book.getPrint_year());
+        preparedStatement.setString(3,book.getAuthor_name());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
