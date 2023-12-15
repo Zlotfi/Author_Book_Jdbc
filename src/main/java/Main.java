9ddcd3265bcb009753ec34fa5a2cbe06f8226e9a
victus <zlotfi758@gmail.com>
2023-12-15 22:21:19 +0330

    add main method for test project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,27 @@
+import menu.Menu;
+import model.Author;
+import model.Book;
+import repository.AuthorRepository;
+import service.AuthorService;
+import service.BookService;
+
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        AuthorService authorService = new AuthorService();
+        BookService bookService = new BookService();
+        Author author = new Author(null,"zahra","lotfi",22, new String[]{"bidarii", "hasti", "siahat"});
+        Author author1 = new Author(null,"farhad","hasan zade",52, new String[]{"ziba sedayam kon", "havij bastani"});
+
+        Book book = new Book(null,"hasti","1380","zahra lotfi");
+//        authorService.register(author1);
+//        authorService.load(2);
+
+//        bookService.addBook(book);
+//        bookService.load();
+//        bookService.delete(2);
+        Menu menu = new Menu();
+        menu.publicMenu();
+    }
 }
