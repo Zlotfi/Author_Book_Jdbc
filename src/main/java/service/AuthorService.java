aeb29b51d1106ad1050fb0293d50b2a5bcc739b7
victus <zlotfi758@gmail.com>
2023-12-15 19:46:28 +0330

    add register method

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -1,4 +1,22 @@
 package service;
 
+import model.Author;
+import repository.AuthorRepository;
+
+import java.sql.SQLException;
+
 public class AuthorService {
+
+    private final AuthorRepository authorRepository = new AuthorRepository();
+
+    public AuthorService() throws SQLException {
+    }
+
+    public void register(Author author) throws SQLException {
+        int result = authorRepository.save(author);
+        if (result != 0)
+            System.out.println(author.getFirstName() + " " + author.getLastName() + " successfully added to database");
+        else
+            System.out.println("ERROR 404");
+    }
 }
