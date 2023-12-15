e60e5ebcb17df9a1028ed4efee949c150c3ede98
victus <zlotfi758@gmail.com>
2023-12-15 20:25:38 +0330

    add addBook method

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,4 +1,22 @@
 package service;
 
+import model.Book;
+import repository.BookRepository;
+
+import java.sql.SQLException;
+
 public class BookService {
+
+    private final BookRepository bookRepository = new BookRepository();
+
+    public BookService() throws SQLException {
+    }
+
+    public void addBook(Book book) throws SQLException {
+        int result = bookRepository.save(book);
+        if (result != 0)
+            System.out.println(book.getTitle() + "successfully added to database");
+        else
+            System.out.println("ERROR 404");
+    }
 }
