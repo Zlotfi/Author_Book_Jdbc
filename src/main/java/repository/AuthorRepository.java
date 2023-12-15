45f49165a7260522e684b9bbbf3276cc96ca8e72
victus <zlotfi758@gmail.com>
2023-12-15 20:09:38 +0330

    add load method

--- src/main/java/repository/AuthorRepository.java
+++ src/main/java/repository/AuthorRepository.java
@@ -5,6 +5,7 @@ import model.Author;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
 
@@ -23,14 +24,33 @@ public class AuthorRepository {
         preparedStatement.setString(2, author.getLastName());
         preparedStatement.setInt(3, author.getAge());
 
-        // تبدیل آرایه bookList به یک رشته جداگانه با استفاده از ویرگول (,)
         String bookListString = String.join(",", author.getBookList());
 
         preparedStatement.setString(4, bookListString);
 
-        // اجرای استعلام و ذخیره سازی در دیتابیس
         int rowsAffected = preparedStatement.executeUpdate();
 
         return rowsAffected;
+    }
+
+    public Author load(int id) throws SQLException {
+        String query = "SELECT * FROM author WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            int authorId = resultSet.getInt("id");
+            String firstName = resultSet.getString("firstName");
+            String lastName = resultSet.getString("lastName");
+            int age = resultSet.getInt("age");
+            String bookListString = resultSet.getString("bookList");
+
+            String[] bookList = bookListString.split(",");
+
+            Author author = new Author(authorId, firstName, lastName, age, bookList);
+            return author;
+        } else {
+            return null;
+        }
     }
 }
